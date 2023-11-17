fc480d762a7a8ff7c9cfc6f219f70802a2b67d8d
Maglb <magalilebon@Magalis-MBP.lan>
2023-11-16 17:57:32 -0700

    Functions for the timer and to display the game have been added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ var questionEl = document.getElementById("question");
 var answerEl = document.getElementById('answer');
 var optionsEl = document.getElementsByClassName("options");
 var timerEl = document.getElementById("timer");
+var questionArea = document.getElementById("questionArea");
+var btnContainer = document.getElementById("btn-container");
+var timeInterval;
+var timeLeft;
+var index = 0;
+var questionItem;
+var nextQ;
 
 var firstQ = {
     prompt: "Inside the HTML document, where do you place your JavaScript code?",
@@ -30,128 +37,123 @@ var FourthQ = {
 // Add all the objects questions in one variable
 var allQuestions = [firstQ, secondQ, thirdQ, FourthQ];
 
-console.log(questionEl);
-console.log(answerEl);
-console.log(optionsEl);
-console.log(typeof optionsEl);
-console.log(allQuestions);
-console.log(typeof allQuestions);
-console.log(timerEl);
-console.log(typeof timerEl);
+// Display headline and intro 
+var introEl = document.createElement("p");
+var intro = questionArea.appendChild(introEl);
 
-var questionArea = document.getElementById("questionArea");
+intro.textContent = "This is a timed quiz. Once you press start you will have 60 seconds to complete the quiz";
 
-// // Display headline and intro 
-// var introEl = document.createElement("p");
-// var intro = questionArea.appendChild(introEl);
+// Create a start button
+var startEl = document.createElement("button");
+var startBtn = questionArea.appendChild(startEl);
+startBtn.innerText = "Start";
 
-// intro.textContent = "This is a timed quiz. Once you press start you will have 60 seconds to complete the quiz";
+// Add an eventListener to the start button to start the quiz and timer.
 
+startBtn.addEventListener('click', timer);
 
-// // Create a start button
-// var startEl = document.createElement("button");
-// var startBtn = questionArea.appendChild(startEl);
-// startBtn.innerText = "Start";
+// Timer that counts down from 60
+function timer() {
 
-// // Add an eventListener to the start button to start the quiz and timer.
+    timeLeft = 60;
 
-// startBtn.addEventListener('click', timer);
+    timeInterval = setInterval(function () {
+        if (timeLeft > 0) {
+            timerEl.textContent = "Timer: " + timeLeft + "s";
+            timeLeft--;
+        } else {
+            timerEl.textContent = "Timer: " + timeLeft + "s";
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+            // Once `timeLeft` gets to 0, display "Game Over"
+            questionEl.textContent = 'Game Over';
+            btnContainer.remove();
+        }
+    }, 1000);
 
-// // IT IS WORKING Timer that counts down from 60
-//     function timer() {
-//         var timeLeft = 60;
-
-//         var timeInterval = setInterval(function () {
-//             if (timeLeft > 0) {
-//                 timerEl.textContent = timeLeft;
-//                 timeLeft--;
-//             } else {
-//                 timerEl.textContent = "0";
-//                 // Use `clearInterval()` to stop the timer
-//                 clearInterval(timeInterval);
-//                 // Once `timeLeft` gets to 0, display "Game Over"
-//                 questionEl.textContent = 'Game Over';
-//             }
-//         }, 1000);
-
-//         startBtn.remove();
-//         intro.remove();
-
-//         // call function to display the quiz
-
-//     };
-
-
-// Function to display the quiz
+    startBtn.remove();
+    intro.remove();
+    createBtnOptions();
+    displayQ();
+};
 
 // For each ol element, add a button element
-
-for (var i = 0; i < optionsEl.length; i++) {
-    var button = document.createElement("button");
-    var optionsBtn = optionsEl[i].appendChild(button);
-    optionsBtn.className = 'optionBtn';
-}
-
-// Timer that counts down from 2 and then upload the next question
-function nextQuestion() {
-    var time = 2;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var countdown = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (time > 1) {
-        time--;
-      } else {
-        clearInterval(countdown);
-        // Call the `displayMessage()` function
-        displayQ();
-      }
-    }, 1000);
-  };
-
-var index = 0;
-var questionItem = allQuestions[index];
-
-// Function to upload question and options on the page
-
-function displayQ() {
-    // Add text to the H2 (#question) element 
-    questionItem = allQuestions[index];
-
-    if (index < allQuestions.length) {
-        questionEl.textContent = questionItem.prompt;
-
-           // For each button inside of the ol element, add the corresponding answer option for the current question
+function createBtnOptions() {
     for (var i = 0; i < optionsEl.length; i++) {
-        optionsEl[i].children[0].textContent = questionItem.options[i];
-    }
-
-    // Add EventListener to the button options and display either the selected option is right or not
-
-    for (var i = 0; i < optionsEl.length; i++) {
-        optionsEl[i].addEventListener('click', function (event) {
-            if (event.target.innerText == questionItem.answer) {
-                answerEl.textContent = "Correct!"
-    
-            } else {
-                answerEl.textContent = "Wrong!"
-            }
-
-            // Once the answer is selected, upload the next question
-            index++;
-            nextQuestion(); 
-        });
-    }
-
-        // Show score
-    } else {
-        questionEl.textContent = "You score is " + timeleft;
-        for (var i = 0; i < optionsEl.length; i++) {
-            optionsEl[i].remove();
-          }
+        var button = document.createElement("button");
+        var optionsBtn = optionsEl[i].appendChild(button);
+        optionsBtn.className = 'optionBtn';
     }
 };
 
-displayQ();
+var buttonEl = document.getElementsByTagName("button");
+console.log(buttonEl);
 
+// Function to enable the options buttons
+function enableButtons() {
+    for (var i = 0; i < optionsEl.length; i++) {
+        optionsEl[i].children[0].disabled = false;
+    }
+};
 
+// Function to disable the options buttons
+function disableButtons() {
+    for (var i = 0; i < optionsEl.length; i++) {
+        optionsEl[i].children[0].disabled = true;
+    }
+};
+
+function displayQ() {
+
+    answerEl.textContent = "";
+    questionItem = allQuestions[index];
+    enableButtons();
+
+    // Add text to the H2 (#question) element 
+    if (index < allQuestions.length) {
+        questionEl.textContent = questionItem.prompt;
+
+        // For each button inside of the ol element, add the corresponding answer option for the current question
+        for (let i = 0; i < optionsEl.length; i++) {
+            optionsEl[i].children[0].textContent = questionItem.options[i];
+            optionsEl[i].addEventListener('click', function (event) {
+                if (event.target.innerText == questionItem.answer) {
+                    answerEl.textContent = "Correct!"
+
+                } else {
+                    answerEl.textContent = "Wrong!"
+                    timeLeft = timeLeft - 10;
+                    if (timeLeft < 0) {
+                        questionEl.textContent = "Game Over\nYour Score is 0";
+                        clearInterval(timeInterval);
+                    }
+                }
+ // Once the answer is selected, upload the next question after a second past and disable the buttons so the answer can't be changed.
+                event.target.onclick = setTimeout(displayQ, 1000);
+                disableButtons();
+            });
+        }
+        index++;
+    }
+
+    else if (timeLeft === 0) {
+
+        // Once `timeLeft` gets to 0, display "Game Over" and the final score.
+        questionEl.textContent = "Game Over\nYour Score is " + timeLeft;
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+        btnContainer.remove();
+    }
+
+    else {
+
+        // Once all the questions have been answered, stop game and display the score
+        questionEl.textContent = 'Your Score is ' + timeLeft;
+        // Stop the timer
+        clearInterval(timeInterval);
+        btnContainer.remove();
+
+    }
+};
+
+// Store scores
